aaa9f43fcff9356f7c88e1ab52e232f877a7f7fa
张紫良 <zzl3_1415926@163.com>
2017-09-10 17:08:27 +0800

    update 3.py

--- 3_ Longest_Substring_Without_Repeating_Characters.py
+++ 3_ Longest_Substring_Without_Repeating_Characters.py
@@ -1,13 +1,28 @@
-#Runtime: 99 ms
+#Approach 1  Sliding Window
 class Solution(object):
 	def lengthOfLongestSubstring(self,s):
-		dic,ans,start = {},0,0
-		for i,ch in enumerate(s):
-			if ch in dic:
-				ans = max(ans,i-start)
-				start = max(start,dic[ch] + 1)
-			dic[ch] = i	
-		return max(ans,len(s) - start)
-s = "abcccc"
-so = Solution()
-print(so.lengthOfLongestSubstring(s))
+		hashSet = set()
+		ans,i,j = 0,0,0
+		while i<len(s) and j < len(s):
+			if s[j] not in hashSet:
+				hashSet.add(s[j])
+				j += 1
+				ans = max(ans,j-i)
+			else:
+				hashSet.remove(s[i])
+				i += 1
+		return ans
+
+#Approach 2 Sliding Window Optimized
+class Solution(object):
+	def lengthOfLongestSubstring(self,s):
+		hashDict ,ans ,i = {},0,0
+		for j ,ch in enumerate(s):
+			if ch in hashDict.keys():
+				ans = max(ans,j-i)
+				i = max(i,hashDict[ch] + 1)
+			hashDict[ch] = j
+		return max(ans,len(s)-i)
+		
+obj = Solution()
+print(obj.lengthOfLongestSubstring("pwwkew"))
